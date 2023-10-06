8a0c0e6ae0fa9e05209db601eed7e36f8e7a42aa
Tanner McDaniel <tmmcdaniel909@gmail.com>
2023-10-06 02:17:48 +0000

    added comments

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,38 +1,47 @@
+// create elements function
 function createElements(){
     const app = document.getElementById("app");
 
+    // h1 element
     const title = document.createElement("h1");
     title.classList.add("d-flex", "justify-content-center", "mt-5");
     title.textContent = "Tic-Tac-Toe";
     app.appendChild(title);
 
+    // row for input elements
     const inputRow = document.createElement("div");
     inputRow.classList.add("row", "pt-5", "justify-content-center");
     app.appendChild(inputRow);
 
+    // input element 1
     const player1NameInput = document.createElement("input");
     player1NameInput.classList.add("col-3", "ms-5", "rounded-pill");
     player1NameInput.placeholder = "Player 1";
     inputRow.appendChild(player1NameInput);
 
+    // input element 2
     const player2NameInput = document.createElement("input");
     player2NameInput.classList.add("col-3", "ms-5", "rounded-pill");
     player2NameInput.placeholder = "Player 2";
     inputRow.appendChild(player2NameInput);
 
+    // row for gameboard tiles
     const gameBoard = document.createElement("div");
     gameBoard.classList.add("row", "mx-auto", "pt-5");
     app.appendChild(gameBoard);
 
+    // for loop to make 9 tiles
     for (let i=0; i<=8; i++){
     const tiles = createTiles(i);
     gameBoard.appendChild(tiles);
     }
 
+    //row for button element
     const buttonRow = document.createElement("div");
     buttonRow.classList.add("row", "justify-content-center", "pt-3");
     app.appendChild(buttonRow);
 
+    // button element
     const restartButton = document.createElement("button");
     restartButton.classList.add("col-2", "justify-content-center", "py-2", "rounded-pill")
     restartButton.textContent = "Restart Game";
@@ -41,6 +50,7 @@ function createElements(){
 
 }
 
+// create tiles function
 function createTiles(index){
     const tileElement = document.createElement("button");
     tileElement.classList.add("col-4", "py-5", "text-center", "tiles");
@@ -49,6 +59,7 @@ function createTiles(index){
     return tileElement;
 }
 
+// restart game function
 function restartGame(){
     for (let i=0; i<=8; i++){
         let tileElement = document.getElementById(i);
@@ -66,6 +77,7 @@ let player2Pieces = [];
 
 let possibleWinCons = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]];
 
+// eval win con function
 function evalWinCon(){
     const app = document.getElementById("app");
     for (let i=0; i<possibleWinCons.length; i++){
@@ -88,6 +100,7 @@ function evalWinCon(){
     }
 }
 
+// play piece function
 function playPiece(event){
     const tileElement = event.target;
     const tileIndex = tileElement.id;
