f9a4621cd310933d0f386c0b2fbf449fce42bbaa
Tanner McDaniel <tmmcdaniel909@gmail.com>
2023-10-06 02:05:02 +0000

    evalWinCon

--- js/sandbox.js
+++ js/sandbox.js
@@ -67,6 +67,7 @@ let player2Pieces = [];
 let possibleWinCons = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]];
 
 function evalWinCon(){
+    const app = document.getElementById("app");
     for (let i=0; i<possibleWinCons.length; i++){
         const winCon = possibleWinCons[i];
         const index0 = winCon[0];
